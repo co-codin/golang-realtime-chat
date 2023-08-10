db9071911a53dfac8bbbabfbf915bee60f589c73
elijah <cuiyeqing960904@gmail.com>
2023-08-10 04:42:09 -0400

    building main , connecting to db

--- db/db.go
+++ db/db.go
@@ -7,5 +7,18 @@ type Database struct {
 }
 
 func NewDatabase(db *sql.DB) (*Database, error) {
-	db, err := sql.Open("postgres", "")
+	db, err := sql.Open("postgres", "postgresql://root:password@localhost:5433/go-chat?sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{db: db}, nil
+}
+
+func (d *Database) Close() {
+	d.db.Close()
+}
+
+func (d *Database) GetDB() *sql.DB {
+	return d.db
 }
