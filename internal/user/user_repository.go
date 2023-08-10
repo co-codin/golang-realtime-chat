26bfa28081142cc0c041201604e8c8d5a44f95d4
elijah <cuiyeqing960904@gmail.com>
2023-08-10 07:22:05 -0400

    user repo

--- internal/user/user_repository.go
+++ internal/user/user_repository.go
@@ -1,1 +1,13 @@
 package user
+
+import (
+	"context"
+	"database/sql"
+)
+
+type DBTX interface {
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+	PrepareContext(context.Context, string) (*sql.Stmt, error)
+	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
+	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
+}
