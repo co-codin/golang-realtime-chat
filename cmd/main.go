0aca3af049bba9d530e0c597faf79a8f50828690
elijah <cuiyeqing960904@gmail.com>
2023-08-10 18:59:54 -0400

    building router

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,21 @@ import (
 	"log"
 
 	"github.com/co-codin/golang-realtime-chat/db"
+	"github.com/co-codin/golang-realtime-chat/internal/user"
+	"github.com/co-codin/golang-realtime-chat/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 
 	if err != nil {
 		log.Fatalf("could not initialize database connection: %s", err)
 	}
 
-	
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHandler(userSvc)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8080")
 }
