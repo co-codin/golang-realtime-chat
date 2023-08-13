6c50eda2897e83f577318770f1f16412796e4ad3
elijah <cuiyeqing960904@gmail.com>
2023-08-13 17:29:31 -0400

    ws run func

--- internal/ws/hub.go
+++ internal/ws/hub.go
@@ -35,7 +35,26 @@ func (h *Hub) Run() {
 				}
 			}
 		case cl := <- h.Unregister:
+			if _, ok := h.Rooms[cl.RoomID]; ok {
+				if _, ok := h.Rooms[cl.RoomID].Clients[cl.ID]; ok {
+					if len(h.Rooms[cl.RoomID].Clients) != 0 {
+						h.Broadcast <- &Message{
+							Content: "user left the chat",
+							RoomID: cl.RoomID,
+							Username: cl.Username,
+						}
+					}
+
+					delete(h.Rooms[cl.RoomID].Clients, cl.ID)
+					close(cl.Message)
+				}
+			}
 		case m := <- h.Broadcast:
+			if _, ok := h.Rooms[m.RoomID]; ok {
+				for _, cl := range h.Rooms[m.RoomID].Clients {
+					cl.Message <- m
+				}
+			}
 		}
 	}
 }
